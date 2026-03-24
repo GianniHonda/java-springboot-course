6c8caae3f54f1e171191ae4eb945c72c0aaf0f23
GianniHonda <gianni.stephanie91@gmail.com>
2026-03-24 18:41:39 +0900

    Initial commit

---
+++ src/main/java/io/github/giannihonda/apiproducts/controller/ProductController.java
@@ -0,0 +1,18 @@
+package io.github.giannihonda.apiproducts.controller;
+
+import io.github.giannihonda.apiproducts.model.Product;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("products")
+public class ProductController {
+
+    @PostMapping
+    public Product save(@RequestBody Product product) {
+        System.out.println("Product received: " + product);
+        return product;
+    }
+}
